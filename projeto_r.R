8de364402587a7cd0e12940f31f0a93605217b8f
rogeriofragoso <rogeriofragoso@gmail.com>
2017-07-02 17:55:12 -0300

    Removendo título dos gráficos

--- projeto_r.R
+++ projeto_r.R
@@ -1,30 +1,32 @@
 # Read CSV into R
+setwd("/Users/rogeriofragoso/Dropbox/PhD/Disciplinas/Estatistica/in1119")
+
 path = getwd()
 path = paste(path, 'data', sep='/')
-afsa = unlist(read.csv(file=paste(path, 'afsa.csv', sep='/'), header=TRUE, sep=','))
-cmfdr = unlist(read.csv(file=paste(path, 'cmfdr.csv', sep='/'), header=TRUE, sep=','))
-mfdr = unlist(read.csv(file=paste(path, 'mfdr.csv', sep='/'), header=TRUE, sep=','))
-mfd = unlist(read.csv(file=paste(path, 'mfd.csv', sep='/'), header=TRUE, sep=','))
+AFSA = unlist(read.csv(file=paste(path, 'afsa.csv', sep='/'), header=TRUE, sep=','))
+cMFDR = unlist(read.csv(file=paste(path, 'cmfdr.csv', sep='/'), header=TRUE, sep=','))
+MFDR = unlist(read.csv(file=paste(path, 'mfdr.csv', sep='/'), header=TRUE, sep=','))
+MFD = unlist(read.csv(file=paste(path, 'mfd.csv', sep='/'), header=TRUE, sep=','))
 
 # Wrap data in a dataframe
-data = data.frame(afsa, cmfdr, mfdr, mfd)
+data = data.frame(AFSA, cMFDR, MFDR, MFD)
 
 # Descriptive statistics
-afsa_mean = mean(afsa)
-afsa_sd = sd(afsa)
-afsa_median = median(afsa)
+afsa_mean = mean(AFSA)
+afsa_sd = sd(AFSA)
+afsa_median = median(AFSA)
 
-cmfdr_mean = mean(cmfdr)
-cmfdr_sd = sd(cmfdr)
-cmfdr_median = median(cmfdr)
+cmfdr_mean = mean(cMFDR)
+cmfdr_sd = sd(cMFDR)
+cmfdr_median = median(cMFDR)
 
-mfdr_mean = mean(mfdr)
-mfdr_sd = sd(mfdr)
-mfdr_median = median(mfdr)
+mfdr_mean = mean(MFDR)
+mfdr_sd = sd(MFDR)
+mfdr_median = median(MFDR)
 
-mfd_mean = mean(mfd)
-mfd_sd = sd(mfd)
-mfd_median = median(mfd)
+mfd_mean = mean(MFD)
+mfd_sd = sd(MFD)
+mfd_median = median(MFD)
 
 cat('Descriptive statistics\n')
 cat('__________________________________________')
@@ -37,62 +39,62 @@ cat(mfd_mean, '\t|', mfd_median, '\t|', mfd_sd)
 cat('__________________________________________')
 
 files_path = paste(getwd(), '/img/', sep = '', col="blue")
-files_path
+
 # Draw histograms for samples
 pdf(paste(files_path, 'hist_afsa.pdf', sep = ''))
-hist(afsa, col="blue")
+hist(AFSA, col="blue", main=NULL)
 dev.off()
 
 pdf(paste(files_path, 'hist_cmfdr.pdf', sep = ''))
-hist(cmfdr, col="blue")
+hist(cMFDR, col="blue", main=NULL)
 dev.off()
 
 pdf(paste(files_path, 'hist_mfdr.pdf', sep = ''))
-hist(mfdr, col="blue")
+hist(MFDR, col="blue")
 dev.off()
 
 pdf(paste(files_path, 'hist_mfd.pdf', sep = ''))
-hist(mfd, col="blue")
+hist(MFD, col="blue", main=NULL)
 dev.off()
 
 # Draw boxplots for samples
 pdf(paste(files_path, 'boxplot.pdf', sep = ''))
-boxplot(data)
+boxplot(data, ylab='Micro-F1 (x100)', main=NULL)
 dev.off()
 
 # Draw normal probability plot for samples
 pdf(paste(files_path, 'norm_afsa.pdf', sep = ''))
-qqnorm(afsa, col="blue", pch=16)
-qqline(afsa, col="green")
+qqnorm(AFSA, col="blue", pch=16, main=NULL)
+qqline(AFSA, col="green")
 dev.off()
 
 pdf(paste(files_path, 'norm_cmfdr.pdf', sep = ''))
-qqnorm(cmfdr, col="blue", pch=16)
-qqline(cmfdr, col="green")
+qqnorm(cMFDR, col="blue", pch=16, main=NULL)
+qqline(cMFDR, col="green")
 dev.off()
 
 pdf(paste(files_path, 'norm_mfdr.pdf', sep = ''))
-qqnorm(mfdr, col="blue", pch=16)
-qqline(mfdr, col="green")
+qqnorm(MFDR, col="blue", pch=16, main=NULL)
+qqline(MFDR, col="green")
 dev.off()
 
 pdf(paste(files_path, 'norm_mfd.pdf', sep = ''))
-qqnorm(mfd, col="blue", pch=16)
-qqline(mfd, col="green")
+qqnorm(MFD, col="blue", pch=16, main=NULL)
+qqline(MFD, col="green")
 dev.off()
 
 
 # Shapiro-Wilk test to verify normality of the samples (default alpha = 0.05)
-shapiro.test(afsa)
-shapiro.test(cmfdr)
-shapiro.test(mfdr)
-shapiro.test(mfd)
+shapiro.test(AFSA)
+shapiro.test(cMFDR)
+shapiro.test(MFDR)
+shapiro.test(MFD)
 
 # Kolmogrov-Smirnov test
-ks.test(afsa, pnorm(10, mean(afsa), sd(afsa)))
-ks.test(cmfdr, pnorm(10, mean(cmfdr), sd(cmfdr)))
-ks.test(mfdr, pnorm(10, mean(mfdr), sd(mfdr)))
-ks.test(mfd, pnorm(10, mean(mfd), sd(mfd)))
+ks.test(AFSA, pnorm(10, mean(AFSA), sd(AFSA)))
+ks.test(cMFDR, pnorm(10, mean(cMFDR), sd(cMFDR)))
+ks.test(MFDR, pnorm(10, mean(MFDR), sd(MFDR)))
+ks.test(MFD, pnorm(10, mean(MFD), sd(MFD)))
 
 
 # Hypothesis test for multiple samples
