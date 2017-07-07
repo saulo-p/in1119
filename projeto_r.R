2d19879f638e59e74d0f338af8fc76e8ad9d4384
rogeriofragoso <rogeriofragoso@gmail.com>
2017-07-07 16:56:04 -0300

    ajustes finais

--- projeto_r.R
+++ projeto_r.R
@@ -1,5 +1,9 @@
-# Load library
+# setwd('<<YOUR WORKING DIRECTORY HERE>>')
+
+Sys.setlocale('LC_CTYPE', 'pt_BR.UTF-8')
+# Load libraries
 library(nortest)
+library(e1071)
 
 # Read CSV into R
 path = getwd()
@@ -13,65 +17,52 @@ MFD = unlist(read.csv(file=paste(path, 'mfd.csv', sep='/'), header=TRUE, sep=','
 
 # Descriptive statistics
 afsa_cmfdr_mean = mean(AFSA_cMFDR)
-afsa_cmfdr_sd = sd(AFSA_cMFDR)
+afsa_cmfdr_sk = skewness(AFSA_cMFDR)
 afsa_cmfdr_median = median(AFSA_cMFDR)
 
 cmfdr_mean = mean(cMFDR)
-cmfdr_sd = sd(cMFDR)
+cmfdr_sk = skewness(cMFDR)
 cmfdr_median = median(cMFDR)
 
 afsa_mfdr_mean = mean(AFSA_MFDR)
-afsa_mfdr_sd = sd(AFSA_MFDR)
+afsa_mfdr_sk = skewness(AFSA_MFDR)
 afsa_mfdr_median = median(AFSA_MFDR)
 
 mfdr_mean = mean(MFDR)
-mfdr_sd = sd(MFDR)
+mfdr_sk = skewness(MFDR)
 mfdr_median = median(MFDR)
 
 afsa_mfd_mean = mean(AFSA_MFD)
-afsa_mfd_sd = sd(AFSA_MFD)
+afsa_mfd_sk = skewness(AFSA_MFD)
 afsa_mfd_median = median(AFSA_MFD)
 
 mfd_mean = mean(MFD)
-mfd_sd = sd(MFD)
+mfd_sk = skewness(MFD)
 mfd_median = median(MFD)
 
 cat('Descriptive statistics\n')
 cat('__________________________________________')
 cat('Mean\t\t&Median\t\t&Std dev', '\\\\\n',
-afsa_cmfdr_mean, '\t&', afsa_cmfdr_median, '\t&', afsa_cmfdr_sd, '\\\\\n',
-cmfdr_mean, '\t&', cmfdr_median, '\t&', cmfdr_sd, '\\\\\n',
-afsa_mfdr_mean, '\t&', afsa_mfdr_median, '\t&', afsa_mfdr_sd, '\\\\\n',
-mfdr_mean, '\t&', mfdr_median, '\t&', mfdr_sd, '\\\\\n',
-afsa_mfd_mean, '\t&', afsa_mfd_median, '\t&', afsa_mfd_sd, '\\\\\n',
-mfd_mean, '\t&', mfd_median, '\t&', mfd_sd, '\\\\')
+afsa_cmfdr_mean, '\t&', afsa_cmfdr_median, '\t&', afsa_cmfdr_sk, '\\\\\n',
+cmfdr_mean, '\t&', cmfdr_median, '\t&', cmfdr_sk, '\\\\\n',
+afsa_mfdr_mean, '\t&', afsa_mfdr_median, '\t&', afsa_mfdr_sk, '\\\\\n',
+mfdr_mean, '\t&', mfdr_median, '\t&', mfdr_sk, '\\\\\n',
+afsa_mfd_mean, '\t&', afsa_mfd_median, '\t&', afsa_mfd_sk, '\\\\\n',
+mfd_mean, '\t&', mfd_median, '\t&', mfd_sk, '\\\\')
 cat('__________________________________________')
 
 files_path = paste(getwd(), '/img/', sep = '', col="blue")
 
 # Draw histograms for samples
-pdf(paste(files_path, 'hist_afsa_cmfdr.pdf', sep = ''))
-hist(AFSA_cMFDR, col="blue", main=NULL, xlab=NULL)
-dev.off()
 
-pdf(paste(files_path, 'hist_cmfdr.pdf', sep = ''))
-hist(cMFDR, col="blue", main=NULL, xlab=NULL)
-dev.off()
-
-pdf(paste(files_path, 'hist_afsa_mfdr.pdf', sep = ''))
-hist(AFSA_MFDR, col="blue", main=NULL, xlab=NULL)
-dev.off()
-
-pdf(paste(files_path, 'hist_mfdr.pdf', sep = ''))
-hist(MFDR, col="blue", main=NULL, xlab=NULL)
-dev.off()
-
-pdf(paste(files_path, 'hist_afsa_mfd.pdf', sep = ''))
-hist(AFSA_MFD, col="blue", main=NULL, xlab=NULL)
-dev.off()
-
-pdf(paste(files_path, 'hist_mfd.pdf', sep = ''))
-hist(MFD, col="blue", main=NULL, xlab=NULL)
+pdf(paste(files_path, 'histograms.pdf', sep = ''))
+par(mfrow=c(3,2))
+hist(AFSA_cMFDR, col="blue", xlab='Micro-F1 (x100)', main='AFSA+cMFDR', breaks=c(80, 80.8, 81.6, 82.4, 83.2, 84))
+hist(cMFDR, col="blue", xlab='Micro-F1 (x100)', main='cMFDR', breaks=c(80, 80.7, 81.4, 82.1, 82.8, 83.5))
+hist(AFSA_MFDR, col="blue", xlab='Micro-F1 (x100)', main='AFSA+MFDR', breaks=c(78.5, 79.2, 79.9, 80.6, 81.3, 82))
+hist(MFDR, col="blue", xlab='Micro-F1 (x100)', main='MFDR', breaks=c(78, 78.8, 79.6, 80.4, 81.2, 82))
+hist(AFSA_MFD, col="blue", xlab='Micro-F1 (x100)', main='AFSA+MFD', breaks=c(79.5, 80.2, 80.9, 81.6, 82.3, 83))
+hist(MFD, col="blue", xlab='Micro-F1 (x100)', main='MFD', breaks=c(80, 80.7, 81.4, 82.1, 82.8, 83.5))
 dev.off()
 
 # Wrap data in a dataframe
@@ -83,33 +74,24 @@ boxplot(data, ylab='Micro-F1 (x100)', main=NULL, cex.axis=0.75)
 dev.off()
 
 # Draw normal probability plot for samples
-pdf(paste(files_path, 'norm_afsa_cmfdr.pdf', sep = ''))
-qqnorm(AFSA_cMFDR, col="blue", pch=16, main=NULL)
+pdf(paste(files_path, 'qqplots.pdf', sep = ''))
+par(mfrow=c(3,2))
+qqnorm(AFSA_cMFDR, col="blue", pch=16, main='AFSA+cMFDR', ylab = 'Quantis amostrais', xlab = 'Quantis teóricos')
 qqline(AFSA_cMFDR, col="green")
-dev.off()
 
-pdf(paste(files_path, 'norm_cmfdr.pdf', sep = ''))
-qqnorm(cMFDR, col="blue", pch=16, main=NULL)
+qqnorm(cMFDR, col="blue", pch=16, main='cMFDR', ylab = 'Quantis amostrais', xlab = 'Quantis teóricos')
 qqline(cMFDR, col="green")
-dev.off()
 
-pdf(paste(files_path, 'norm_afsa_mfdr.pdf', sep = ''))
-qqnorm(AFSA_MFDR, col="blue", pch=16, main=NULL)
+qqnorm(AFSA_MFDR, col="blue", pch=16, main='AFSA+MFDR', ylab = 'Quantis amostrais', xlab = 'Quantis teóricos')
 qqline(AFSA_MFDR, col="green")
-dev.off()
 
-pdf(paste(files_path, 'norm_mfdr.pdf', sep = ''))
-qqnorm(MFDR, col="blue", pch=16, main=NULL)
+qqnorm(MFDR, col="blue", pch=16, main='MFDR', ylab = 'Quantis amostrais', xlab = 'Quantis teóricos')
 qqline(MFDR, col="green")
-dev.off()
 
-pdf(paste(files_path, 'norm_afsa_mfd.pdf', sep = ''))
-qqnorm(AFSA_MFD, col="blue", pch=16, main=NULL)
+qqnorm(AFSA_MFD, col="blue", pch=16, main='AFSA+MFD', ylab = 'Quantis amostrais', xlab = 'Quantis teóricos')
 qqline(AFSA_MFD, col="green")
-dev.off()
 
-pdf(paste(files_path, 'norm_mfd.pdf', sep = ''))
-qqnorm(MFD, col="blue", pch=16, main=NULL)
+qqnorm(MFD, col="blue", pch=16, main='MFD', ylab = 'Quantis amostrais', xlab = 'Quantis teóricos')
 qqline(MFD, col="green")
 dev.off()
 
